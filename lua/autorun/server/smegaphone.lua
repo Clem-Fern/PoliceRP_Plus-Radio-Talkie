eb340609b8e04824fcf6cf264d3b991ab267d583
Clem-Fern <clemfern456@gmail.com>
2020-12-06 20:21:22 +0100

    Fix voice hook nil ActiveWeapon

--- lua/autorun/server/smegaphone.lua
+++ lua/autorun/server/smegaphone.lua
@@ -3,7 +3,7 @@ include("autorun/megaphone.lua")
 
 // Voice Hook
 hook.Add("PlayerCanHearPlayersVoice","PoliceRPPlusMegaphoneVoice",function(listener,speaker)
-    if(speaker:Alive() and speaker:GetActiveWeapon():GetClass() == "wep_megaphone") then
+    if((speaker:Alive()) and (speaker:GetActiveWeapon() != NULL ) and (speaker:GetActiveWeapon():GetClass() == "wep_megaphone")) then
         if(megaphone.isInRange(speaker,listener))then
             return true
         end
